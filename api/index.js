7a1067f425fc6bf269be985c1fcfd061452701fa
mika-store <mikastore.co.id@gmail.com>
2026-07-29 10:13:13 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -46,7 +46,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: ['https://mikastore.vercel.app', 'http://localhost:3000'],
+  origin: ['https://mikastore-7oij.vercel.app', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
